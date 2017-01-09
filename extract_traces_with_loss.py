b411097ec7a0a142cf6ec6c8c13d619ae6233243
Francis Y. Yan <francisyyan@stanford.edu>
2017-01-09 14:40:12 -0800

    extract_traces_with_loss.py: extract loss events

--- extract_traces_with_loss.py
+++ extract_traces_with_loss.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import json
 import argparse
 from os import path
@@ -33,40 +34,73 @@ def main():
             trace_path = path.join(
                 args.data_dir, '%s_datalink_run%s.loss.trace' % (cc, run_id))
             log = open(log_path)
-            trace = open(trace_path, 'w')
 
-            queue = deque()
             first_ts = None
+            queue = []
+            delivery = []
             for line in log:
                 if line.startswith('#'):
                     continue
 
                 items = line.split()
                 ts = float(items[0])
+                if first_ts is None:
+                    first_ts = ts
 
                 if items[1] == '+':
                     queue.append(ts)
                 elif items[1] == '-':
-                    if first_ts is None:
-                        first_ts = int(ts) - 1
+                    delivery.append(ts)
 
                     delay = float(items[3])
+                    sent_ts = ts - delay
+
+                    i = 0
                     while True:
-                        queue_head = queue.popleft()
-                        sent_ts = ts - delay
-                        if abs(sent_ts - queue_head) <= 0.002:
-                            trace.write('%s\n' % (int(ts) - first_ts))
+                        if i >= len(queue):
                             break
-                        elif sent_ts > queue_head:
-                            trace.write('%s x\n' % (int(ts) - first_ts))
-                        else:
-                            sys.stderr.write('Cannot find sent packet for %s'
-                                             % line)
-                            exit(1)
+
+                        if abs(sent_ts - queue[i]) <= 0.005:
+                            del queue[i]
+                            break
+
+                        i += 1
 
             log.close()
-            trace.close()
 
+            trace = open(trace_path, 'w')
+            for ts in xrange(1, int(first_ts)):
+                trace.write('%s\n' % ts)
+
+            p1 = 0 # queue
+            p2 = 0 # delivery
+
+            while p1 < len(queue) or p2 < len(delivery):
+                if p1 < len(queue):
+                    ts1 = queue[p1]
+                else:
+                    ts1 = None
+
+                if p2 < len(delivery):
+                    ts2 = delivery[p2]
+                else:
+                    ts2 = None
+
+                if ts1 and ts2:
+                    if ts1 < ts2:
+                        trace.write('%s x\n' % int(round(ts1)))
+                        p1 += 1
+                    else:
+                        trace.write('%s\n' % int(round(ts2)))
+                        p2 += 1
+                elif ts1:
+                    trace.write('%s x\n' % int(round(ts1)))
+                    p1 += 1
+                elif ts2:
+                    trace.write('%s\n' % int(round(ts2)))
+                    p2 += 1
+
+            trace.close()
 
 if __name__ == '__main__':
     main()
