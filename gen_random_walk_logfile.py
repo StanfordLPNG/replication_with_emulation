683a8103238d6113a27be3aa14cb62caec0dfdad
Greg D. Hill <greghill@cs.stanford.edu>
2017-01-10 12:12:30 -0800

    biased randwalk trace

--- gen_random_walk_logfile.py
+++ gen_random_walk_logfile.py
@@ -5,8 +5,8 @@ total_time = 1000 * 30  # 60 sec trace
 cur_time = 1
 
 # can't go above 12
-mbps_max = 10.2
-mbps_min = 8.7
+mbps_max = 9.8
+mbps_min = 7.3
 
 def mbps_to_ppms(mbps):
     return ((mbps/8.)*1024.*1024.)/(1000.*1500.)
@@ -17,7 +17,7 @@ min_packets_per_ms = mbps_to_ppms(mbps_min)
 cur_packets_per_ms = max_packets_per_ms
 
 # every 100ms decide how many departure events to have
-with open('randwalk_trace.log', 'w') as trace:
+with open('randwalk.trace', 'w') as trace:
     while cur_time < total_time:
         rnd = random.random()
         #print('Random: %.4f cur_packets_per_ms %.4f' % (rnd, cur_packets_per_ms))
@@ -25,11 +25,10 @@ with open('randwalk_trace.log', 'w') as trace:
             #print('%d' % cur_time)
             trace.write('%d\n' % cur_time)
 
-        probability_delta = .002
         if random.getrandbits(1):
-            cur_packets_per_ms += probability_delta
+            cur_packets_per_ms += .003
         else:
-            cur_packets_per_ms -= probability_delta
+            cur_packets_per_ms -= .002
 
         cur_packets_per_ms = min(cur_packets_per_ms, max_packets_per_ms)
         cur_packets_per_ms = max(cur_packets_per_ms, min_packets_per_ms)
